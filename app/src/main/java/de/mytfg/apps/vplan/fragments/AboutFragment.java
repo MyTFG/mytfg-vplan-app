2b20c13f123a4ed63422706ba5720f603c19e941
Lennart Bader <mail@lbader.de>
2016-12-30 12:49:32 +0100

    Removed useless imports from AboutFragment

--- app/src/main/java/de/mytfg/apps/vplan/fragments/AboutFragment.java
+++ app/src/main/java/de/mytfg/apps/vplan/fragments/AboutFragment.java
@@ -1,34 +1,22 @@
 package  de.mytfg.apps.vplan.fragments;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 import  de.mytfg.apps.vplan.R;
 import  de.mytfg.apps.vplan.activities.MainActivity;
-import de.mytfg.apps.vplan.api.SuccessCallback;
-import de.mytfg.apps.vplan.objects.Vplan;
-import  de.mytfg.apps.vplan.toolbar.ToolbarManager;
 
 public class AboutFragment extends Fragment {
-    private View view;
     private final String githubUrl = "https://github.com/MyTFG/mytfg-vplan-app";
 
 
@@ -38,7 +26,7 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
         MainActivity context = (MainActivity)this.getActivity();
         setHasOptionsMenu(true);
         context.getToolbarManager()
