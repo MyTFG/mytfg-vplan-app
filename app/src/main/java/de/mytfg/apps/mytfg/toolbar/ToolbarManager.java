c880002d8552b118d8f2b16a5df09c4f622a5be1
Lennart Bader <mail@lbader.de>
2017-06-02 19:14:54 +0200

    Toolbar expands on showSearchBar

--- app/src/main/java/de/mytfg/apps/mytfg/toolbar/ToolbarManager.java
+++ app/src/main/java/de/mytfg/apps/mytfg/toolbar/ToolbarManager.java
@@ -35,6 +35,7 @@ public class ToolbarManager {
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private TabLayout tabs;
     private SearchView searchView;
+    private boolean currentTabOutscroll;
 
     private Toolbar toolbar;
 
@@ -101,8 +102,10 @@ public class ToolbarManager {
         Settings settings = new Settings(context);
         boolean autohide_tabs = settings.getBool("hide_toolbar");
         if (outscroll && autohide_tabs) {
+            currentTabOutscroll = true;
             params.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_ENTER_ALWAYS_COLLAPSED | AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL);
         } else {
+            currentTabOutscroll = false;
             params.setScrollFlags(AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED | AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL);
         }
         collapsingToolbarLayout.setLayoutParams(params);
@@ -213,6 +216,9 @@ public class ToolbarManager {
     }
 
     public ToolbarManager showSearchBar() {
+        if (currentTabOutscroll) {
+            this.setExpandable(true, true);
+        }
         MenuItem searchItem = toolbar.getMenu().findItem(R.id.action_search);
         if (searchItem != null) {
             searchItem.expandActionView();
